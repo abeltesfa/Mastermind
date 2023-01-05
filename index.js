ecb6fc2fbf0d03ce86a92f0431a084675ec4ef95
Abel Tesfa <abeltesfa@gmail.com>
2023-01-05 01:46:37 -0500

    random api call added

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const numSelector = document.querySelectorAll('.opt');
 const addSelection = document.querySelector('.current');
 
+async function generateRandom() {
+    let response = await fetch('https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new');
+    let data = await response.text()
+    return data;
+}
+
+generateRandom().then(data=> console.log(data))
+
+// fetch('https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new/')
+//     .then(response => {
+//         return response.text();
+//     })
+//     .then(randomNums => {
+//         console.log(randomNums)
+//     })
+
 numSelector.forEach(num => {
     const selNum = num.classList[1];
     num.addEventListener('click', () => selectNumber(selNum))
