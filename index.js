312ddabd35206a5c915bafa465a843256dfe3415
Abel Tesfa <abeltesfa@gmail.com>
2023-01-05 16:55:09 -0500

    hints system sort of working

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function generateRandom() {
     return data;
 }
 
-generateRandom().then(data=> data.split('\n').slice(0,4).map((ranNum)=> randomAnswer.push(+ranNum)));
+generateRandom().then(data=> data.split('\n').slice(0,4).map((ranNum)=> randomAnswer.push(ranNum)));
 
 console.log(randomAnswer)
 
@@ -37,7 +37,57 @@ function selectNumber(selNum) {
             hintDiv.setHTML(selSelNum);
             feedbackSection.appendChild(hintDiv)
         }
+
+        const hintsArr = generateHints(selectedNums);
+        let hintsObj = {};
+        const completeHintDiv = document.createElement('div');
+
+        for(let k = 0; k < hintsArr.length; k++){
+            if(hintsObj[hintsArr[k]] === undefined){
+                hintsObj[hintsArr[k]] = 1;
+            } else hintsObj[hintsArr[k]] += 1;
+        }
+
+        if(hintsObj['right'] && hintsObj['almost']){
+            completeHintDiv.setHTML(`There are ${hintsObj['right']} numbers in the correct position and ${hintsObj['almost']} numbers that are not in the correct location`);
+            feedbackSection.appendChild(completeHintDiv)
+        } else if (hintsObj['right']) {
+            completeHintDiv.setHTML(`There are ${hintsObj['right']} numbers in the correct position`);
+            feedbackSection.appendChild(completeHintDiv)
+        } else if (hintsObj['almost']){
+            completeHintDiv.setHTML(`There are ${hintsObj['almost']} numbers that are not in the correct location`);
+            feedbackSection.appendChild(completeHintDiv)
+        } else {
+            completeHintDiv.setHTML(`There are no correct numbers`);
+            feedbackSection.appendChild(completeHintDiv)
+        }
+
+        console.log(hintsArr)
+        console.log(hintsObj)
+
+
         selectedNums.length = 0;
         addSelection.innerHTML = "";
     }
+}
+
+
+function generateHints(pickedNums) {
+    const hints = [];
+    const dups = [];
+
+    pickedNums.forEach((num, i) => {
+        if(randomAnswer[i] === num) {
+            hints.push('right');
+            dups.push(num);
+        }
+    });
+
+    pickedNums.forEach((num, i) => {
+        if(!dups.includes(num) && randomAnswer.includes(num)){
+            hints.push('almost')
+        }
+    })
+
+    return hints
 }
